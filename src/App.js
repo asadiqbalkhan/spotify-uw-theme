65a2740753ee819bd9d84852c6b23045e53e46a2
asadiqbalkhan <iq.asad@gmail.com>
2021-07-14 07:25:15 +0500

    context setup for user and token

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
-
+import { DataLayer, useDataLayerValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState();
+  const [{ user }, dispatch] = useDataLayerValue();
   // run code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,15 +17,24 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
-        console.log("🙇", user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
-    console.log(" I HAVE A TOKEN :arrow: ", token);
-  });
+    console.log(" I HAVE A TOKEN 🔥  ", token);
+  }, []);
 
+  console.log("🙇", user);
+  console.log("👾", token);
   return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
