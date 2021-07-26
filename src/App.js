e5660ea04270bad2ee30d5ee20c19e6747138ef4
asadiqbalkhan <iq.asad@gmail.com>
2021-07-26 18:33:12 +0500

    UI components completed , need to work on UW theme

--- src/App.js
+++ src/App.js
@@ -38,6 +38,13 @@ function App() {
           playlists: playlists,
         });
       });
+
+      spotify.getPlaylist("37i9dQZEVXcNaqSlEt3YH7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
     }
     console.log(" I HAVE A TOKEN 🔥  ", token);
   }, []);
--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,40 @@
+import { Favorite, PlayCircleFilled } from "@material-ui/icons";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import React from "react";
 import "./Body.css";
+import { useDataLayerValue } from "./DataLayer";
 import Header from "./Header";
+import SongRow from "./SongRow";
 
 const Body = ({ spotify }) => {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
   return (
     <>
       <div className="body">
         <Header spotify={spotify} />
+
+        {/* Banner */}
+        <div className="body__info">
+          <img src={discover_weekly?.images[0].url} alt="discover_weekly" />
+          {/* console.log({discover_weekly?.images[0].url}) */}
+          <div className="body__infoText">
+            <strong>PLAYLIST</strong>
+            <h2>Discover Weekly</h2>
+            <p>{discover_weekly?.description}</p>
+          </div>
+        </div>
+        <div className="body__songs">
+          <div className="body__icons">
+            <PlayCircleFilled className="body__shuffle" />
+            <Favorite fontSize="large" />
+            <MoreHorizIcon />
+          </div>
+          {/* List of songs */}
+          {discover_weekly?.tracks.items.map((item) => (
+            <SongRow track={item.track} />
+          ))}
+        </div>
       </div>
     </>
   );
